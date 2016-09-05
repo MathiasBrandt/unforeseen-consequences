ba5d8d5ea049cd3d85dbe5ded177ab2009cb1e70
Per Mortensen <per@permortensen.com>
2016-09-05 20:15:24 +0200

    Initial Xamarin + MonoGame project

---
+++ Android/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Content.PM;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Microsoft.Xna.Framework;
+
+namespace UnforeseenConsequences.Droid
+{
+    [Activity(Label = "UnforeseenConsequences.Droid",
+              MainLauncher = true,
+              Icon = "@drawable/icon",
+              Theme = "@style/Theme.Splash",
+              AlwaysRetainTaskState = true,
+              LaunchMode = LaunchMode.SingleInstance,
+              ConfigurationChanges = 
+                                     ConfigChanges.KeyboardHidden |
+                                     ConfigChanges.Keyboard)]
+    public class MainActivity : AndroidGameActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Create our OpenGL view, and display
+            var g = new Game1();
+            SetContentView((View)g.Services.GetService(typeof(View)));
+            g.Run();
+        }
+
+    }
+}
+
+
